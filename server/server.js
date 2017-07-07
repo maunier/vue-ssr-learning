df1e9d3d539eb5c9b20f876dabd13948390eee0a
赵瑞 <zhaorui@zhaoruideMacBook-Pro.local>
2017-07-07 18:58:11 +0800

    一个ssr示例

--- server/server.js
+++ server/server.js
@@ -1,13 +1,43 @@
 const Koa = require('koa');
-const static = require('koa-static');
-const path = require('path');
-// const Router = require('koa-router');
+const Vue = require('vue');
+const renderer = require('vue-server-renderer').createRenderer();
+const Router = require('koa-router');
+
+// const static = require('koa-static');
+// const path = require('path');
 
 const app = new Koa();
-const staticPath = path.resolve(__dirname, '../web');
-// const router = new Router();
+const router = new Router();
 
-app.use(static(staticPath));
+router.get('*', ctx => {
+  const app = new Vue({
+    data: {
+      username: 'zhaorui'
+    },
+    template: `<div>hello {{username}}, this is a page rendered at server!</div>`
+  });
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      console.log(err)
+      ctx.response.status(500).end('Internet server error');
+      return;
+    }
+    ctx.body = `
+      <DOCTYPE html>
+      <html>
+        <head></head>
+        <body>${html}</body>
+      </html>
+
+    `;
+  });
+});
+
+app.use(router.routes());
+// const staticPath = path.resolve(__dirname, '../web');
+
+// app.use(static(staticPath));
 
 // app.use(ctx => {
 //   ctx.body = 'hello koa2';
