c6ce34d2e04ed5b6e2c01cbf1d58066fd96351a7
zhaorui <zhaorui140280@163.com>
2017-07-26 20:44:40 +0800

    feat(webpack): 配置服务端的nodeexternals,不要把三方包打包进bundle

--- build/webpack.config.server.babel.js
+++ build/webpack.config.server.babel.js
@@ -2,6 +2,7 @@ import merge from 'webpack-merge';
 import path from 'path';
 import baseConfig from './webpack.config.base.babel.js';
 import VueSSRServerPlugin from 'vue-server-renderer/server-plugin'
+import nodeExternals from 'webpack-node-externals';
 
 const config = merge(baseConfig, {
   target: 'node',
@@ -15,6 +16,7 @@ const config = merge(baseConfig, {
       'create-api': './create-api-server.js',
     }
   },
+  externals: nodeExternals(),
   plugins: [
     // 这是将服务器的整个输出
     // 构建为单个 JSON 文件的插件。
