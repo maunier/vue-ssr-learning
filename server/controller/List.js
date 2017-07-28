8107b793f0d97caccf2fa87743d41411f5d625da
zhaorui <zhaorui140280@163.com>
2017-07-28 14:43:58 +0800

    一个简单的mongodb例子

--- server/controller/List.js
+++ server/controller/List.js
@@ -3,16 +3,7 @@ import Article from '../model/Article';
 export default class List {
   static async getList(ctx) {
     const articles = await Article.getAllArticles();
-    const response = {
-      category1: [{
-        id: 1,
-        title: `${articles}1`
-      }],
-      category2: [{
-        id: 2,
-        title: `${articles}2`
-      }]
-    }
-    ctx.response.body = response[ctx.params.type];
+ 
+    ctx.response.body = articles;
   }
 }
--- server/dao/Articles.js
+++ server/dao/Articles.js
@@ -1,19 +1,27 @@
 import redis from 'redis';
 import bluebird from 'bluebird';
+import { MongoClient } from 'mongodb';
+import { host } from '../../constants';
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 export default class ArticlesDao {
   constructor () {
+    const url = `mongodb://${host}:27017/my-blog`;
+
     this.client = redis.createClient();
+    this.db = MongoClient.connect(url);
   }
 
   async getAllArticles() {
-    return await this.client.get('articles');
+    const db = await this.db;
+    const articleCollection = db.collection('articles');
+    const result = await articleCollection.find({}).toArray();
+
+    return result;
   }
 
   async addOneArticle(article) {
-    return await this.client.set('articles:', article);
   }
 }
